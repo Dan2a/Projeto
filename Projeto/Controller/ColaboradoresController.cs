116b6fe85fac4cad3a9cba382e0d3014c48ecf49
Dan2a <dan.andrade313@gmail.com>
2022-05-02 09:14:51 -0300

    Att

---
+++ Projeto/Controller/ColaboradoresController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Projeto.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColaboradoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext
+        // GET: api/<ColaboradoresController>
+        [HttpGet]
+        public IEnumerable<UserListDo> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ColaboradoresController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ColaboradoresController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ColaboradoresController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ColaboradoresController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
