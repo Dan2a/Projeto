d59f672bf5905b378379aa4779e2ea0e76fb6dca
Dan2a <dan.andrade313@gmail.com>
2022-05-02 21:40:38 -0300

    Verificação de login

---
+++ Projeto/Controllers/authController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto.Models;
+using Projeto.Models.Dtos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Projeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class authController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public authController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<authController>
+        [HttpGet]
+        public bool Get(LoginDto login)
+        {
+            var user = _context.Colaboradores.FirstOrDefault(c => c.Email == login.Email);
+            if (user is not null)
+                return user.Senha == login.Senha;
+            return false;
+        }
+
+    }
+}
--- Projeto/Models/Dtos/LoginDto.cs
+++ Projeto/Models/Dtos/LoginDto.cs
@@ -2,9 +2,7 @@
 {
     public class LoginDto
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
+        public string Email { get; set; }
         public string Senha { get; set; }
     }
 }
